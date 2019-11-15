269dc59d90df1599cf57f2177a1bf47fdca3df20
levg34 <levg34@users.noreply.github.com>
2019-11-16 00:51:40 +0100

    ça marche

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var svg = d3.select('#svg1').append('svg')
 
-function drawPoint(x,y) {
-	svg.append("circle")
-		.attr("cx",x)
-		.attr("cy",y)
-		.attr("r",5);
+function drawPoint(x,y,color) {
+	svg.append('circle')
+		.attr('cx',x)
+		.attr('cy',y)
+		.attr('r',5)
+		.style('fill', color)
 }
 	
 function quadrille(a,L) {
 	for (var n=1; n*a<L; n++) {
 		for (var m=1;2*a*n-m*a>0;m++) {
-			drawPoint(m*a,2*a*n-m*a)
+			if (m*a < L && 2*a*n-m*a < L)
+				drawPoint(m*a,2*a*n-m*a,'lightgrey')
 		}
 	}
 }
 
-drawPoint(20,150)
-quadrille(20,150)
+drawPoint(150,150,'red')
+quadrille(20,300)
