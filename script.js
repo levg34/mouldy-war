f7ded9f9d194711530ddf25e06d809ac86f48dde
levg34 <guilhem.dross@laposte.net>
2019-11-23 01:50:55 +0100

    un semblant de truc qui marche !

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var canvas = document.getElementById('myCanvas')
+var context = canvas.getContext('2d')
+
 function quadrille(cote,hauteur,largeur) {
 	if (!largeur) largeur = hauteur
 	var res = []
@@ -18,17 +21,46 @@ function chaos(points,amplitude) {
 	})
 	return res
 }
--
-console.log(chaos(quadrille(20,500)))
-
-var c = document.getElementById('myCanvas')
-var context = c.getContext('2d')
 
 const points = chaos(quadrille(20,500,960),20)
 const delaunay = Delaunay.from(points)
 const voronoi = delaunay.voronoi([0, 0, 960, 500])
-console.log(voronoi)
-context.beginPath();
-voronoi.render(context);
-// context.strokeStyle = "#fff";
-context.stroke();
+
+function tracer(point) {
+	context.clearRect(0,0,canvas.width,canvas.height)
+	
+	// voronoi.update()
+	
+	context.beginPath()
+	voronoi.render(context)
+	context.strokeStyle = '#00008B'
+	context.stroke()
+	
+	if (point) {
+		context.beginPath()
+		voronoi.renderCell(5, context)
+		context.fillStyle = '#663399'
+		context.fill()
+		
+		context.beginPath()
+		context.arc(point[0], point[1], 5, 0, 2 * Math.PI, true)
+		context.fillStyle = '#4169E1'
+		context.fill()
+	}
+}
+
+canvas.onmousemove = event => {
+    event.preventDefault()
+	
+	var rect = canvas.getBoundingClientRect()
+    var souris = [event.clientX - rect.left, event.clientY - rect.top]
+	tracer(souris)
+	// voronoi.contains(i, x, y)
+    // tracer()
+}
+
+function dessinerPoint(point) {
+	
+}
+
+tracer()
