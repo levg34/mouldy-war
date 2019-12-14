c3c5fdb00c765372493286e8f8162d086ea04c83
levg34 <levg34@users.noreply.github.com>
2019-12-14 02:28:26 +0100

    mise en place de nodejs, expressjs et socket.io

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/vue/index.html')
+})
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' })
+	socket.on('my other event', function (data) {
+		console.log(data)
+	})
+})
+
+server.listen(80, () => console.log('Serveur démarré sur le port 80.'))
+// WARNING: app.listen(80) will NOT work here!
